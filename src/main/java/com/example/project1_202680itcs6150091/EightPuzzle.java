81e5294dc4e328371b5d200722926f295eece0e2
Daniel Klingensmith <danielpklingensmith@gmail.com>
2026-09-03 22:45:50 -0400

    Updated to track path and node generation/expansion

--- src/main/java/com/example/project1_202680itcs6150091/EightPuzzle.java
+++ src/main/java/com/example/project1_202680itcs6150091/EightPuzzle.java
@@ -6,74 +6,181 @@ import java.io.IOException;
 import java.util.*;
 
 public class EightPuzzle {
+
     private final HeuristicFunction heuristicFunction;
 
     public EightPuzzle(HeuristicFunction heuristicFunction) {
         this.heuristicFunction = heuristicFunction;
     }
 
-    private int getTotalEstimatedH(Integer[][] puzzle){
+    public static class RunResults {
+        private final List<Integer[][]> solutionPath;
+        private final int nodesGenerated;
+        private final int nodesExpanded;
+
+        public RunResults(
+                List<Integer[][]> path,
+                int nodesGenerated,
+                int nodesExpanded) {
+
+            this.solutionPath = path;
+            this.nodesGenerated = nodesGenerated;
+            this.nodesExpanded = nodesExpanded;
+        }
+
+        public List<Integer[][]> getPath() {
+            return solutionPath;
+        }
+
+        public int getNodesGenerated() {
+            return nodesGenerated;
+        }
+
+        public int getNodesExpanded() {
+            return nodesExpanded;
+        }
+
+        @Override
+        public String toString() {
+            return "RunResults{" +
+                    "solutionPath=" + solutionPath +
+                    ", nodesGenerated=" + nodesGenerated +
+                    ", nodesExpanded=" + nodesExpanded +
+                    '}';
+        }
+    }
+
+    private int getTotalEstimatedH(Integer[][] puzzle) {
         int total = 0;
-        for(int i =0; i<3;i++){
-            for(int j=0;j<3;j++) {
-                if(puzzle[i][j]!=0) {
-                    total += heuristicFunction.getHValue(new Coordinate(j,i), puzzle[i][j]);
+
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                if (puzzle[y][x] != 0) {
+                    total += heuristicFunction.getHValue(
+                            new Coordinate(x, y),
+                            puzzle[y][x]
+                    );
                 }
             }
         }
+
         return total;
     }
 
-    public Integer[][] solvePuzzle(Integer[][] initialState){
-        System.out.println("Beginning puzzle: \n"+stringifyPuzzle(initialState));
+    public RunResults solvePuzzle(Integer[][] initialState) {
+        System.out.println("Beginning puzzle:\n"+stringifyPuzzle(initialState));
+        /*
+         *   key = (g, h)
+         *   value = puzzle state
+         *   Priority is f (g+h)
+         */
+        PriorityQueue<Pair<Pair<Integer, Integer>, Integer[][]>> frontier =
+                new PriorityQueue<>(
+                        Comparator.comparingInt(pair -> pair.getKey().getKey()
+                                        + pair.getKey().getValue()));
+
         int initialHValue = getTotalEstimatedH(initialState);
-        //Pair<Integer, Integer> is (gValue, hValue), AKA (distance travelled, heuristic estimate of distance remaining)
-        PriorityQueue<Pair<Pair<Integer, Integer>, Integer[][]>> frontier = new PriorityQueue<>(Comparator.comparingInt(pair -> (pair.getKey().getKey() + pair.getKey().getValue())));
-        frontier.add(new Pair<>(new Pair<>(0, initialHValue), initialState));
+
+        frontier.add(new Pair<>(new Pair<>(0, initialHValue), deepCopyArray(initialState)));
+
+        String initialKey = stringifyPuzzle(initialState);
+
         Map<String, Integer> bestG = new HashMap<>();
-        Pair<Pair<Integer, Integer>, Integer[][]> latestState=null;
-        while(!frontier.isEmpty() ) {
-            latestState = frontier.poll();
+        bestG.put(initialKey, 0);
 
-            //If h is zero, we're there
-            if(latestState.getKey().getValue() == 0){
-                System.out.println("Final state: \n"+ stringifyPuzzle(latestState.getValue()));
-                System.out.println("Goal achieved in "+latestState.getKey().getKey()+" steps\n");
-                return latestState.getValue();
+        Map<String, String> stateParent = new HashMap<>();
+        stateParent.put(initialKey, null);
+
+        Map<String, Integer[][]> visitedStates = new HashMap<>();
+        visitedStates.put(initialKey, deepCopyArray(initialState));
+
+        int nodesGenerated = 1;
+        int nodesExpanded = 0;
+
+        while (!frontier.isEmpty()) {
+            Pair<Pair<Integer, Integer>, Integer[][]> current = frontier.poll();
+
+            Integer[][] currentState = current.getValue();
+            int currentG = current.getKey().getKey();
+            String currentKey = stringifyPuzzle(currentState);
+
+            /*
+             * If state already exists with shorter path, skip this loop
+             */
+            if (currentG > bestG.get(currentKey)) {
+                continue;
             }
-            List<Pair<Pair<Integer, Integer>, Integer[][]>> possibleMoves = getPossibleStates(latestState.getValue(), latestState.getKey().getKey());
-            for(Pair<Pair<Integer, Integer>, Integer[][]> pair : possibleMoves){
-                String stringState = stringifyPuzzle(pair.getValue());
-                int gValue = pair.getKey().getKey();
 
-                if (!bestG.containsKey(stringState) || gValue < bestG.get(stringState)) {
-                    bestG.put(stringState, gValue);
+            if (getTotalEstimatedH(currentState)==0) {
+                List<Integer[][]> solutionPath = reconstructPath(currentKey, stateParent, visitedStates);
+                System.out.println("Final state: "+stringifyPuzzle(currentState));
+                System.out.println("Goal achieved in " + currentG + " steps");
+                System.out.println("Nodes generated: " + nodesGenerated);
+                System.out.println("Nodes expanded: " + nodesExpanded);
+                System.out.println("Solution path:");
+
+                for (Integer[][] integers : solutionPath) {
+                    System.out.println(stringifyPuzzle(integers));
+                }
+                return new RunResults(solutionPath, nodesGenerated, nodesExpanded);
+            }
+
+            nodesExpanded++;
+            List<Pair<Pair<Integer, Integer>, Integer[][]>> possibleMoves = getPossibleStates(currentState, currentG);
+            nodesGenerated += possibleMoves.size();
+
+            for (Pair<Pair<Integer, Integer>, Integer[][]> pair : possibleMoves) {
+                Integer[][] successor = pair.getValue();
+                int successorG = pair.getKey().getKey();
+                String successorKey = stringifyPuzzle(successor);
+                /*
+                 * Make sure this is the best path to this state in the queue
+                 */
+                if (!bestG.containsKey(successorKey) || successorG < bestG.get(successorKey)) {
+                    bestG.put(successorKey, successorG);
+                    stateParent.put(successorKey, currentKey);
+                    visitedStates.put(successorKey, deepCopyArray(successor));
                     frontier.add(pair);
                 }
             }
         }
-        System.out.println("Goal unachievable- stopped at state:\n"+stringifyPuzzle(latestState.getValue()));
-        return initialState;
+        System.out.println("Goal is unreachable.");
+        System.out.println("Nodes generated: " + nodesGenerated);
+        System.out.println("Nodes expanded: " + nodesExpanded);
+        return new RunResults(Collections.emptyList(), nodesGenerated, nodesExpanded);
+    }
+
+    private List<Integer[][]> reconstructPath(String goal, Map<String, String> parent, Map<String, Integer[][]> states) {
+        String currentKey = goal;
+        List<Integer[][]> path = new ArrayList<>();
+
+        while (currentKey != null) {
+            path.add(deepCopyArray(states.get(currentKey)));
+            currentKey = parent.get(currentKey);
+        }
+
+        Collections.reverse(path);
+        return path;
     }
 
     private List<Pair<Pair<Integer, Integer>, Integer[][]>> getPossibleStates(Integer[][] state, int distanceTravelled) {
         Coordinate emptyPosition = findEmptyPosition(state);
         List<Pair<Pair<Integer, Integer>, Integer[][]>> options = new ArrayList<>();
 
-        if(emptyPosition.getX()-1>=0 ){
-            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX()-1,emptyPosition.getY()), emptyPosition);
+        if (emptyPosition.getX() - 1 >= 0) {
+            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX() - 1, emptyPosition.getY()), emptyPosition);
             options.add(new Pair<>(new Pair<>(distanceTravelled + 1, getTotalEstimatedH(newState)), newState));
         }
-        if(emptyPosition.getX()+1<=2){
-            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX()+1,emptyPosition.getY()), emptyPosition);
+        if (emptyPosition.getX() + 1 <= 2) {
+            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX() + 1, emptyPosition.getY()), emptyPosition);
             options.add(new Pair<>(new Pair<>(distanceTravelled + 1, getTotalEstimatedH(newState)), newState));
         }
-        if(emptyPosition.getY()-1>=0){
-            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX(),emptyPosition.getY()-1), emptyPosition);
+        if (emptyPosition.getY() - 1 >= 0) {
+            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX(), emptyPosition.getY() - 1), emptyPosition);
             options.add(new Pair<>(new Pair<>(distanceTravelled + 1, getTotalEstimatedH(newState)), newState));
         }
-        if(emptyPosition.getY()+1<=2){
-            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX(),emptyPosition.getY()+1), emptyPosition);
+        if (emptyPosition.getY() + 1 <= 2) {
+            Integer[][] newState = moveTile(state, new Coordinate(emptyPosition.getX(), emptyPosition.getY() + 1), emptyPosition);
             options.add(new Pair<>(new Pair<>(distanceTravelled + 1, getTotalEstimatedH(newState)), newState));
         }
 
@@ -88,38 +195,46 @@ public class EightPuzzle {
     }
 
     public static Integer[][] deepCopyArray(Integer[][] original) {
-        if (original == null) return null;
+        if (original == null) {
+            return null;
+        }
         Integer[][] copy = new Integer[original.length][];
         for (int i = 0; i < original.length; i++) {
             if (original[i] != null) {
                 copy[i] = new Integer[original[i].length];
-                for (int j = 0; j < original[i].length; j++) {
-                    if (original[i][j] != null) {
-                        copy[i][j] = original[i][j];
-                    }
-                }
+                System.arraycopy(original[i], 0, copy[i], 0, original[i].length);
             }
         }
         return copy;
     }
 
     private Coordinate findEmptyPosition(Integer[][] puzzle) {
-        for(int i =0; i<3;i++){
-            for(int j=0;j<3;j++) {
-                if(puzzle[i][j]==0) {
-                   return new Coordinate(j, i);
+        for (int y = 0; y < 3; y++) {
+            for (int x = 0; x < 3; x++) {
+                if (puzzle[y][x] == 0) {
+                    return new Coordinate(x, y);
                 }
             }
         }
         throw new IllegalArgumentException("No empty space located in puzzle");
     }
 
+    public static String stringifyPuzzle(Integer[][] puzzle) {
+        StringBuilder result = new StringBuilder();
+        for (Integer[] puzzleNumbers : puzzle) {
+            for (Integer number : puzzleNumbers) {
+                result.append(number);
+                result.append(" ");
+            }
+            result.append("\n");
+        }
+        return result.toString();
+    }
+
     public static void main(String... args) throws IOException {
         Map<Integer, Coordinate> solutionMap = PuzzleParser.generateSolutionMap("TestFiles/DefaultSolution.txt");
-
         HeuristicFunction manhattanHeuristic = new ManhattanHeuristic(solutionMap);
         EightPuzzle manhattanEightPuzzle = new EightPuzzle(manhattanHeuristic);
-
         HeuristicFunction absoluteDistance = new AbsoluteDistanceHeuristic(solutionMap);
         EightPuzzle absoluteDistanceEightPuzzle = new EightPuzzle(absoluteDistance);
 
@@ -141,30 +256,13 @@ public class EightPuzzle {
         tests.add(test7);
         tests.add(test8);
 
-
         System.out.println("Running manhattan heuristic");
         for(Integer[][]test: tests){
             manhattanEightPuzzle.solvePuzzle(test);
         }
-
         System.out.println("Running absolute distance heuristic");
         for(Integer[][]test: tests){
             absoluteDistanceEightPuzzle.solvePuzzle(test);
         }
-
-
     }
-
-    public static String stringifyPuzzle(Integer[][] puzzle) {
-        StringBuilder result = new StringBuilder();
-        for (Integer[] puzzleNumbers : puzzle) {
-            for (Integer number: puzzleNumbers) {
-                result.append(number);
-                result.append(" ");
-            }
-            result.append("\n");
-        }
-        return result.toString();
-    }
-
 }
